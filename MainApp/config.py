5e79680aa2f79999502ee67dd9d2423f6b81251b
mani-barathi <manibarathi.s@gmail.com>
2020-11-11 15:09:31 +0530

    home page modified

--- MainApp/config.py
+++ MainApp/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
 	SECRET_KEY = 'Thisisasecret'
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = '587'
 	MAIL_USE_TLS = True
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from MainApp import app
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True,threaded=True)
